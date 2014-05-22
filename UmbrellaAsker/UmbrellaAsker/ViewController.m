fb7229ac00ac4143f830057ecd6169d88ebc60f3
e125747 <e125747@ie.u-ryukyu.ac.jp>
2014-05-22 12:18:24 +0900

    CocoaPods入れたけどエラー出る

--- UmbrellaAsker/UmbrellaAsker/ViewController.m
+++ UmbrellaAsker/UmbrellaAsker/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController ()
 
@@ -18,6 +20,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    // OpenWeatherMapの天気出力APIエンドポイント
+    NSString *kOpenWeatherMap3HoursForecastAPI = @"http://api.openweathermap.org/data/2.5/weather?units=metric&lat=%f&lon=%f";
+    
+    // 適当な緯度経度AF
+    CLLocationDegrees latitude = 39.01;
+    CLLocationDegrees longitude = 141.68;
+    
+    NSString *urlString = [NSString stringWithFormat:kOpenWeatherMap3HoursForecastAPI, latitude, longitude];
+    
+    [[AFHTTPRequestOperationManager manager] GET:urlString
+                                      parameters:nil
+                                         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                                             NSLog(@"JSON: %@", responseObject);
+                                         }
+                                         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                             NSLog(@"Error: %@", error);
+                                         }];
 }
 
 - (void)didReceiveMemoryWarning
